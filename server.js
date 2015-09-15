da13703a742bbe697019050426448e905f435959
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2015-09-15 12:55:31 +0200

    bugfix

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var port = process.env.PORT || 13337;
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-});
+}).listen(port);
