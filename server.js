51ad7643b93cf441d8da3381035a8a8793a70d7d
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2015-09-15 15:32:57 +0200

    refactored code

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require('http');
 var port = process.env.PORT || 13337;
 
-http.createServer(function(req, res) {
+var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(port);
+});
+
+// Make server list on 'port'
+server.listen(port);
